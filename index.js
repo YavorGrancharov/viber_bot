3ff53d853befa51255d0d47e1cb9ba217afe7510
Yavor <bearhold@gmail.com>
2021-08-01 13:21:28 +0300

    changed publuc url to one that was provided by heroku

--- index.js
+++ index.js
@@ -97,10 +97,8 @@ app.get('/', function (req, res) {
 app.use('/viber/webhook', bot.middleware());
 app.listen(settings.port, async () => {
   try {
-    const publicUrl = await ngrok.getPublicUrl();
     console.log(`Application running on port: ${settings.port}`);
-    console.log('publicUrl => ', publicUrl);
-    bot.setWebhook(`${publicUrl}/viber/webhook`);
+    bot.setWebhook(`https://vbr-bot.herokuapp.com/viber/webhook`);
   } catch (error) {
     console.log('Can not set webhook on following server.');
     console.error(error);
