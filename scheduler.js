4c15037202519b80c0b7f9dbf076fe92a1dd830e
Yavor <bearhold@gmail.com>
2021-08-09 16:31:32 +0300

    removed node cron

--- scheduler.js
+++ scheduler.js
@@ -2,15 +2,12 @@ const userController = require('./src/controllers/consolidator').user;
 const msgController = require('./src/controllers/consolidator').message;
 const ethController = require('./src/controllers/consolidator').ethereum;
 const btcController = require('./src/controllers/consolidator').bitcoin;
-const cron = require('node-cron');
 
 module.exports = () => {
-  cron.schedule('0 12 * * *', async () => {
-    // Save latest ETH, BTC prices to DB on every 24h
-    btcController.savePrice();
-    ethController.savePrice();
+  // Save latest ETH, BTC prices to DB on every 24h
+  btcController.savePrice();
+  ethController.savePrice();
 
-    // Send daily price to subscribers
-    msgController.sendSubscribersDailyMsg(userController);
-  });
+  // Send daily price to subscribers
+  msgController.sendSubscribersDailyMsg(userController);
 };
