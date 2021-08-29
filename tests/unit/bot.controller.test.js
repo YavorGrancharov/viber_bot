1f55ce259c80f36afe60af0a93499b24d8daf0ce
Yavor <bearhold@gmail.com>
2021-08-29 17:50:51 +0300

    testing tests ci workflow

--- tests/unit/bot.controller.test.js
+++ tests/unit/bot.controller.test.js
@@ -4,15 +4,27 @@ const botController = require('../../src/controllers/botController');
 const bot = require('../../src/config/bot')();
 
 bot.on = jest.fn();
+bot.sendMessage = jest.fn();
+
+beforeAll(() => {
+  botController.onConversationStarted(bot);
+});
 
 describe('botController.onConversationStarted', () => {
   it('Should have an onConversationStarted function', () => {
     expect(typeof botController.onConversationStarted).toBe('function');
   });
   it('Should call bot.on event', () => {
-    botController.onConversationStarted();
+    expect(bot.on).toBeCalled();
+  });
+  it('Should call bot.sendMessage function', () => {
     bot.on(BotEvents.CONVERSATION_STARTED, () => {
       expect(bot.sendMessage).toBeCalled();
+    });
+  });
+  it('Should verify response object', () => {
+    bot.on(BotEvents.CONVERSATION_STARTED, (response) => {
+      expect(response).toHaveProperty('userProfile');
     });
   });
 });
